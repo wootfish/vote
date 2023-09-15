d6fed97f9c35dad67fc37dd7d7183570e07be512
Eli Sohl <elisohl@gmail.com>
2023-09-14 19:29:54 -0700

    fixed typo

--- python/vote.py
+++ python/vote.py
@@ -59,7 +59,7 @@ class Vote:
     @staticmethod
     def _ft_autocmd(raw_path):
         tree = (Path(raw_path).expanduser() / "*").as_posix()
-        viim.command(f'autocmd BufRead {tree} setlocal filetype=markdown')
+        vim.command(f'autocmd BufRead {tree} setlocal filetype=markdown')
 
     def ensure_folders(self):
         paths = [self.conf.root_dir] + list((self.conf.extra_dirs or {}).values())
